886f4680e02802c590d9e19eb1505fac4a1af57d
erencan.celik <erencan.celik@enuygun.com>
2023-01-22 14:36:52 +0300

    Added tailwind css packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
+          <p className="text-3xl font-bold underline">
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
