e277700dd578a26f8a4599fd17f3176f3701db78
erencan.celik <erencan.celik@enuygun.com>
2023-01-28 15:05:31 +0300

    .gitignore is now working

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,17 @@
 export interface NavigationProps {
-    isLogin: Boolean;
-  }
+  isLogin: Boolean;
+}
+
+export interface User {
+  jwt: string;
+  user: {
+    id: number;
+    username: string;
+    email: string;
+    provider: string;
+    confirmed: boolean;
+    blocked: boolean;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
